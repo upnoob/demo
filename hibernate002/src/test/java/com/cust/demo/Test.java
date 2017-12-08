5992c7677b4272d721fb252304fafad5b4f6dc23
lx <upnoob@163.com>
2017-12-08 21:30:56 +0800

    update hibernateDemo resources

--- hibernate002/src/test/java/com/cust/demo/Test.java
+++ hibernate002/src/test/java/com/cust/demo/Test.java
@@ -11,7 +11,8 @@ public class Test {
 	
 	public static void main(String[] args) {
 //		add();
-		queryStudentsByGrade();
+//		queryStudentsByGrade();
+		update();
 	}
 
 	public static void add(){
@@ -31,6 +32,9 @@ public class Test {
 		HibernateUtil.closeSession();
 	}
 	
+	/**
+	 * 查询班级和学生信息
+	 */
 	public static void queryStudentsByGrade() {
 		Session session = HibernateUtil.getSession();
 		Grade grade = (Grade) session.get(Grade.class, 1);
@@ -40,6 +44,26 @@ public class Test {
 		for(Student s: stus){
 			System.out.println(s.getSname() + "\t" + s.getGid() + "\t" + s.getSex());
 		}
+		
+	}
+	
+	
+	/**
+	 * 修改学生信息
+	 */
+	public static void update() {
+		Grade g = new Grade();
+		g.setGname("java 二班");
+		g.setGdesc("java 基础班级");
+		
+		Session session = HibernateUtil.getSession();
+		Transaction ta = session.beginTransaction();
+		Student stu = (Student) session.get(Student.class, 1);
+		g.getStudents().add(stu);
+		session.save(stu);
+		ta.commit();
+		HibernateUtil.closeSession();
+		
 		
 	}
 }
