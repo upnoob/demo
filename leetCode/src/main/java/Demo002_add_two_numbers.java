36b7949a009bc44bdd78b95e8dfd7f954c48917c
lx <upnoob@163.com>
2018-01-02 22:14:59 +0800

    add all

--- leetCode/src/main/java/Demo002_add_two_numbers.java
+++ leetCode/src/main/java/Demo002_add_two_numbers.java
@@ -1,6 +1,15 @@
+import java.util.LinkedList;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Created by upnoob on 2017/12/27.
  */
 public class Demo002_add_two_numbers {
+    public static void main(String[] args) {
 
+    }
+
+    public LinkedList<Integer> addTwoNumbers(){
+        throw new IllegalAccessError();
+    }
 }
